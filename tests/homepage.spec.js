eb10bae4d289b2aa321b7bc372f2d1ffa848e40e
avyanbhattacharya <64517985+avyanbhattacharya@users.noreply.github.com>
2026-09-02 23:27:01 -0700

    Keep homepage navigation regression coverage current

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,18 +1,26 @@
 const { test, expect } = require('@playwright/test');
 
-test('homepage exposes the current tools and navigation works', async ({ page }) => {
+test('homepage exposes every current tool and navigation works', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle(/Clean Local Tools/);
   await expect(page.getByRole('heading', { name: /simple tools/i })).toBeVisible();
-  const passport = page.getByRole('link', { name: /passport photo/i });
-  const japa = page.getByRole('link', { name: /japa counter/i });
-  const compress = page.getByRole('link', { name: /compress pdf/i });
-  const merge = page.getByRole('link', { name: /merge pdf/i });
-  const resize = page.getByRole('link', { name: /resize & compress image/i });
-  for (const link of [passport,japa,compress,merge,resize]) await expect(link).toBeVisible();
-  await passport.click(); await expect(page).toHaveURL(/\/passport-photo\/$/); await expect(page.getByRole('heading', { name: /free passport photo maker/i })).toBeVisible();
-  await page.goto('/'); await japa.click(); await expect(page).toHaveURL(/\/japa-counter\/$/); await expect(page.getByRole('heading', { name: /touchless japa counter/i })).toBeVisible();
-  await page.goto('/'); await compress.click(); await expect(page).toHaveURL(/\/compress-pdf\/$/); await expect(page.getByRole('heading', { name: /^compress pdf$/i })).toBeVisible();
-  await page.goto('/'); await merge.click(); await expect(page).toHaveURL(/\/merge-pdf\/$/); await expect(page.getByRole('heading', { name: /^merge pdf$/i })).toBeVisible();
-  await page.goto('/'); await resize.click(); await expect(page).toHaveURL(/\/resize-image\/$/); await expect(page.getByRole('heading', { name: /resize & compress image/i })).toBeVisible();
+
+  const tools = [
+    [/passport photo/i, /\/passport-photo\/$/, /passport photo/i],
+    [/japa counter/i, /\/japa-counter\/$/, /touchless japa counter/i],
+    [/compress pdf/i, /\/compress-pdf\/$/, /^compress pdf$/i],
+    [/merge pdf/i, /\/merge-pdf\/$/, /^merge pdf$/i],
+    [/resize & compress image/i, /\/resize-image\/$/, /resize & compress image/i],
+    [/clean pdf printer/i, /\/clean-pdf-printer\/$/, /clean pdf printer/i],
+    [/document flattener/i, /\/document-flattener\/$/, /document flattener/i]
+  ];
+
+  for (const [linkName] of tools) await expect(page.getByRole('link', { name: linkName })).toBeVisible();
+
+  for (const [linkName, url, heading] of tools) {
+    await page.goto('/');
+    await page.getByRole('link', { name: linkName }).click();
+    await expect(page).toHaveURL(url);
+    await expect(page.getByRole('heading', { name: heading }).first()).toBeVisible();
+  }
 });
